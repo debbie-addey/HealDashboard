7a514ca25c9d33bfba4cdbe6b6dcdd7bd5c90eac
debbie-addey <deborah.addey@dal.ca>
2025-10-29 13:45:03 -0300

    Update HEAL Graphs.py

--- HEAL Graphs.py
+++ HEAL Graphs.py
@@ -55,7 +55,7 @@ df_consented = df[df["consent_complete"] == "2"].copy()
 # Count total consented participants
 consented_count = df_consented.shape[0]
 
-
+st.write(df_consented["participation_status"].value_counts(dropna=False))
 
 # Recall 1 invited = HEAL completed
 recall1_invited = (df["heal_qx_complete"] == "2").sum() if "heal_qx_complete" in df.columns else 0
@@ -218,6 +218,7 @@ if "act_qx_date" in df.columns and "redcap_event_name" in df.columns:
     )
     fig_act.update_layout(xaxis_title="Recall", yaxis_title="Number of Participants")
     st.plotly_chart(fig_act)
+
 
 
 
